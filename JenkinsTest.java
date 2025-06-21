a85f9b0b6a1df134c9acaf7da298a8a22913ab91
Sameer Siddiqui <sameersiddiqui1240@gmail.com>
2025-06-21 23:29:14 +0530

    java test with jenkins

---
+++ JenkinsTest.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+public class JenkinsTest{
+public static void main(String [] args){
+System.out.println("This is java program...");
+System.out.println("Hello jenkins user...");
+System.out.println("output from java program...");
+System.out.println(new Date());
+}
+}
